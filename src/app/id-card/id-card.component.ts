b1580ab3e293511bec1b4d72af575e21cfc9e310
BlackB34rd <contact@the-tiny-spark.com>
2026-05-27 10:59:11 +0200

    correction TP n°1

---
+++ src/app/id-card/id-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-id-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './id-card.component.html',
+  styleUrl: './id-card.component.css'
+})
+export class IdCardComponent {
+  public name: string = 'Legrand';
+  public firstName: string = 'Nicolas';
+  public age: number = 40;
+  public sex: string = 'M';
+}
