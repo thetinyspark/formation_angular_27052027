41d5f462c12d1bfec87b73ee340fc3db7c36d2f2
BlackB34rd <contact@the-tiny-spark.com>
2026-05-27 10:31:43 +0200

    explication scope d'éxécution des fonctions

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -10,5 +10,23 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyApp';
+  public title: string = 'MyApp';
+
+  constructor() {
+
+    // bind sur les objets de type function permet de créer une copie de la fonction
+    // reliée à un contexte spécifique (le this dans la fonction) et de retourner cette copie.
+    this.myFunction = this.myFunction.bind(this);
+
+    this.myFunction(); // Logs the AppComponent instance
+    setTimeout(this.myFunction, 1000);
+  }
+
+  // on a donc une fonction qui est liée à l'instance de AppComponent, même lorsqu'elle est appelée dans un contexte différent (comme dans setTimeout). Cela garantit que le this à l'intérieur de myFunction fait toujours référence à l'instance de AppComponent, ce qui permet d'accéder aux propriétés et méthodes de la classe sans perdre le contexte.
+  // comme alternative, on pourrait aussi utiliser une fonction fléchée 
+  // public myFunction = (): void => {
+  
+  public myFunction (): void {
+    console.log(this);
+  }
 }
