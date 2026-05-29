543d42c4ee1cc8eed9e8367a81e1880bee7fc4cc
BlackB34rd <contact@the-tiny-spark.com>
2026-05-29 14:59:43 +0200

    add cypress example

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,28 @@
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+  it('Visits the initial project page and check title', () => {
     cy.visit('/')
     cy.contains('MyApp')
   })
+
+  it('Visits the catalog and select a platform then checked all products platform', () => {
+    cy.visit('/catalog')
+    cy.get("select").select("N64");
+    cy.get(".product").contains("N64");
+  });
+
+  it('Visits the catalog and search products with a certain name', () => {
+    cy.visit('/catalog')
+    cy.get("#search").type("Call Of");
+    cy.get(".product h2").contains("Call Of");
+  });
+
+  it('should add a product to the cart then should be able to retrieve it', () => {
+    cy.visit('/catalog')
+    cy.get(".product button").first().click();
+    cy.visit('/cart')
+    cy.get(".product h2").contains("Call Of");
+
+  });
+
+
 })
--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -5,18 +5,7 @@ import { Product } from '../model/product';
 import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root', 
-  useFactory: ()=>{
-    // je mets ma logique qui me permet de décider si je renvoie une nouvelle instance
-    // ou une déjà existante
-
-    // if( condition1 ){
-    //   alors je renvoie la même instance générale pour tout le monde
-    // }
-    // else{
-    //   return new CatalogService();
-    // }
-  }
+  providedIn: 'root'
 })
 export class CatalogService {
   public httpClient = inject(HttpClient);
